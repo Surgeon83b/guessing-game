b0a7c98be3d001c1e5784a87c6c27ec9cd437017
Surgeon83b <89166001+Surgeon83b@users.noreply.github.com>
2022-08-13 12:47:31 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+	
+	    constructor() {}
 
     setRange(min, max) {
-
+			this.minVal = min;
+			this.maxVal = max;
     }
 
     guess() {
-
+			return Math.round((this.maxVal + this.minVal)/2);
     }
 
     lower() {
-
+			this.setRange(this.minVal, this.guess()); 
     }
 
     greater() {
-
+			this.setRange(this.guess(), this.maxVal); 
     }
 }
 
